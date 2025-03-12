1f0cbac76ad318445053124d34d61f2d6f2c0e17
Ratchapon Pockathum <61614162+ozoneRatchapon@users.noreply.github.com>
2025-03-12 07:04:39 +0000

    comment out unused LLM imports and instances in lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -10,8 +10,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_groq import ChatGroq
-from langchain_ollama import ChatOllama
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_ollama import ChatOllama
+# from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 llm = ChatGroq(
@@ -19,19 +19,19 @@ llm = ChatGroq(
     temperature=0,
 )
 
-localLLM = ChatOllama(
-    model="llama3.2:1b",
-    temperature=0,
-)
+# localLLM = ChatOllama(
+#     model="llama3.2:1b",
+#     temperature=0,
+# )
 
-googleLLM = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-    # other params...
-)
+# googleLLM = ChatGoogleGenerativeAI(
+#     model="gemini-1.5-pro",
+#     temperature=0,
+#     max_tokens=None,
+#     timeout=None,
+#     max_retries=2,
+#     # other params...
+# )
 
 response = llm.invoke("Write a poem about AI")
 print(response)
